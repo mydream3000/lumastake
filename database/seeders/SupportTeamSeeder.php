700354c98ccc4794c17c0964375ddd81fdba6a76
udodv <zzzakazzz2@gmail.com>
2026-01-17 09:56:37 +0700

    Refactor SupportTeamSeeder to use updateOrCreate for idempotency

--- database/seeders/SupportTeamSeeder.php
+++ database/seeders/SupportTeamSeeder.php
@@ -14,24 +14,30 @@ class SupportTeamSeeder extends Seeder
     public function run(): void
     {
         // Создать email-адреса для поддержки
-        SupportEmail::create([
-            'email' => 'inquiries@lumastake.com',
-            'name' => 'General Inquiries',
-            'is_active' => true,
-        ]);
+        SupportEmail::updateOrCreate(
+            ['email' => 'inquiries@lumastake.com'],
+            [
+                'name' => 'General Inquiries',
+                'is_active' => true,
+            ]
+        );
 
-        SupportEmail::create([
-            'email' => 'support@lumastake.com',
-            'name' => 'Technical Support',
-            'is_active' => true,
-        ]);
+        SupportEmail::updateOrCreate(
+            ['email' => 'support@lumastake.com'],
+            [
+                'name' => 'Technical Support',
+                'is_active' => true,
+            ]
+        );
 
         // Создать Telegram бота
-        TelegramBot::create([
-            'name' => 'Lumastake Info Bot',
-            'bot_token' => '8500874442:AAEEBx4-z32wzlb70UoqZT4pdkYFCfy2r-A',
-            'chat_id' => '', // Нужно заполнить в админке
-            'is_active' => false, // Деактивирован до заполнения chat_id
-        ]);
+        TelegramBot::updateOrCreate(
+            ['name' => 'Lumastake Info Bot'],
+            [
+                'bot_token' => '8500874442:AAEEBx4-z32wzlb70UoqZT4pdkYFCfy2r-A',
+                'chat_id' => '', // Нужно заполнить в админке
+                'is_active' => false, // Деактивирован до заполнения chat_id
+            ]
+        );
     }
 }

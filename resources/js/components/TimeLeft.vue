225db1d140cafe484cc514f3e2f608c1b7a6e5c7
udodv <zzzakazzz2@gmail.com>
2026-01-20 20:54:00 +0100

    Refactor tiers display: consolidate tiers logic into the header, remove redundant implementations from dashboard and rewards views, and adjust layout for header spacing.

--- resources/js/components/TimeLeft.vue
+++ resources/js/components/TimeLeft.vue
@@ -1,6 +1,7 @@
 а<template>
   <span :class="badgeClass">
-   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+   <svg class="time-left" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
 <mask id="mask0_1_8879" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
 <rect width="24" height="24" fill="url(#pattern0_1_8879)"/>
 </mask>

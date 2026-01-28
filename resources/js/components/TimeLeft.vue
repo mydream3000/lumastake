9fcbdd86ebd3a70fe3b6c648ce3d5a771fc3dc7e
udodv <zzzakazzz2@gmail.com>
2026-01-28 14:28:29 +0100

    Refactor email notifications to use centralized templated mail class and improve balance sync behavior.

--- resources/js/components/TimeLeft.vue
+++ resources/js/components/TimeLeft.vue
@@ -1,4 +1,4 @@
-<template>
+a<template>
   <span :class="badgeClass">
 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <mask id="mask0_1_8879" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
